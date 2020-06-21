3d36e78ea88fde2ca4e0bbbf0f1784cf804b5773
numanarif <numanarif87@gmail.com>
2020-06-21 18:16:52 +0700

    penambahan homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:papantulis/login.dart';
+import './homepage.dart';
 
 import 'signup.dart';
 
@@ -18,14 +18,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return LoginPage(); 
-  }
-}
